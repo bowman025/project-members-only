5fadb767ab296d54d2833da0175d8e51354691e3
RstK <208619198+bowman025@users.noreply.github.com>
2026-04-02 15:09:07 +0200

    Update the seed function, change file structure slightly

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+const pool = require('./db/pool');
+const passport = require('./config/passport');
 
 const indexRouter = require('./routes/indexRouter');
+const authRouter = require('./routes/authRouter');
+const messageRouter = require('./routes/messageRouter');
 const CustomNotFoundError = require('./errors/CustomNotFoundError');
 
 const path = require('node:path');
 const assetsPath = path.join(__dirname, 'public');
 
+const app = express();
+
 app.set('views', path.join(__dirname, '/views/pages'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  store: new pgSession({
+    pool: pool,
+    tableName: 'session'
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+}));
+app.use(passport.session());
 app.use(express.static(assetsPath));
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use('/', indexRouter);
 
@@ -26,7 +48,7 @@ app.use((err, req, res, next) => {
   console.error(err);
   const statusCode = err.statusCode || 500;
   res.status(statusCode).render('error', {
-    title: 'The Game Inventory: Error',
+    title: 'The *** Club: Error',
     message: err.message,
   });
 });
--- db/populateDb.js
+++ db/populateDb.js
@@ -23,6 +23,16 @@ const SQL = `
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_id INTEGER REFERENCES users(id) ON DELETE CASCADE 
   );
+
+  CREATE TABLE IF NOT EXISTS "session" (
+    "sid" VARCHAR NOT NULL COLLATE "default",
+    "sess" JSON NOT NULL,
+    "expire" TIMESTAMP(6) NOT NULL
+  ) WITH (OIDS=FALSE);
+
+  ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
+
+  CREATE INDEX IF NOT EXISTS "IDX_session_expire" ON "session" ("expire");
 `;
 
 const users = [
